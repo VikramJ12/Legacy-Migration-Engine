7699d31bece61c89c23f21c39f7061d018a874c7
Prajwalkadam29 <160503760+Prajwalkadam29@users.noreply.github.com>
2025-08-26 18:14:52 +0530

    Create tmp_input.c

---
+++ outputs/tmp_input.c
@@ -0,0 +1,15 @@
+void bubbleSort(int arr[], int n){
+    int i, j;
+    int swapped;  // instead of bool
+    for (i = 0; i < n - 1; i++) {
+        swapped = 0;
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
+                swapped = 1;
+            }
+        }
+        if (swapped == 0)
+            break;
+    }
+}
